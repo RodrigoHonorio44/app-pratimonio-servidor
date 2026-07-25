07b2b3dd6e1eed2a2ce52ff25903d5c03f3b93e6
RodrigoHonorio44 <rodrigohono43@outlook.com>
2026-07-25 16:48:22 -0300

    25.07

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,12 +1,11 @@
 import axios from 'axios';
-import { auth } from './firebase'; // Importa sua configuração do firebase
+import { auth } from './firebase';
 
 const api = axios.create({
-  // Adicionamos o /api aqui para que você não precise repetir nas chamadas
-  baseURL: 'http://192.168.0.232:3000/api', 
+  // Use o domínio público HTTPS do seu túnel do Cloudflare
+  baseURL: 'https://api.rodhonsystem.com.br/api', 
 });
 
-// Interceptor para injetar o token do Firebase em todas as chamadas
 api.interceptors.request.use(async (config) => {
   const user = auth.currentUser;
   if (user) {
